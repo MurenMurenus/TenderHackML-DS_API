ca81a3dff28ce5d1ec1dd43bc9310c19373326cd
MurenMurenus <murenmurenus@gmail.com>
2023-03-31 10:28:46 +0300

    Refactored.

---
+++ src/controllers.py
@@ -0,0 +1,47 @@
+import flask
+from flask import request, jsonify
+import joblib
+import traceback
+import pandas as pd
+import numpy as np
+
+from src.models import pipeline
+
+
+lr = joblib.load("src/models/model.pkl")  # Load pkl of our model.
+print('Model loaded')
+model_columns = joblib.load("src/models/model_columns.pkl")  # Load columns that our model uses.
+print('Model columns loaded')
+
+
+async def async_predict() -> flask.Response:
+    if lr:
+        try:
+            json_ = request.json
+            print(json_)
+            data = await pipeline.pipeline(pd.DataFrame([json_]))
+            prediction = lr.predict(data)[0]
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print('Train the model first')
+        return jsonify({'trace': str('Train the model first')})
+
+
+async def get_metrics() -> flask.Response:
+    if lr:
+        try:
+            json_ = request.json
+            print(json_)
+            data = pipeline.pipeline(pd.DataFrame([json_]))
+            statistics = 'Test statistics'
+            return jsonify({'metrics': str(statistics)})
+
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print('Train the model first')
+        return jsonify({'trace': str('Train the model first')})
